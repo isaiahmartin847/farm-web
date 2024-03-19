3ae01d525bbf4ebbbabd6a8bffb5b416ff8eed1f
isaiah <isaiahmartin847@gmail.com>
2024-03-19 17:38:12 -0600

    stopped displaying the id

--- src/app/components/plat-list.js
+++ src/app/components/plat-list.js
@@ -3,7 +3,6 @@ export default function PlantList ({name, id, price}) {
         <div>
             <h1>{name}</h1>
             <h2>Price ${price}</h2>
-            <h3>Id: {id}</h3>
         </div>
     )
 }
