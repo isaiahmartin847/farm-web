5f71092028c09fda4111acb345cb9b18b3204242
isaiah <isaiahmartin847@gmail.com>
2024-03-20 08:52:04 -0600

    added the styling to the header

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -1,9 +1,14 @@
 export default function NavBar () {
     return (
         <nav className="bg-blue-500">
-            <ul>
-                <li>Plant list</li>
-            </ul>
+            <div className="flex justify-between items-center">
+                <div className="flex item-center">
+                    <h1>NAME</h1>
+                </div>
+                <div className="flex items-center">
+                    <a>Go to cart</a>
+                </div>
+            </div>
         </nav>
     )
 }
