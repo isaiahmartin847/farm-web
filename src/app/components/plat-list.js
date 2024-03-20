e2ed568a12da94cd30d7899657de3ace2a9b0958
isaiah <isaiahmartin847@gmail.com>
2024-03-20 14:49:56 -0600

    added the color dots to the app

--- src/app/components/plat-list.js
+++ src/app/components/plat-list.js
@@ -1,13 +1,14 @@
 import moreBtn from "./more-btn-function";
 
-export default function PlantList ({name, id, price, url}) {
+export default function PlantList ({name, id, price, url, color, colorCode}) {
     return ( 
         <div key={id} className="border-2  m-0.5 flex flex-col justify-center items-center">
             <div className="m-0.5 p-3">
                 <h1 className="mr-1 text-lg text-center" >{name}</h1>
                 <ul className="ml-5 pb-2">
                     <li>Price: {price}</li>
-                    <li>test</li>
+                    <li><div className="flex flex-row">Color: {color} <div className={`w-4 h-4 bg-${colorCode} mr-2 rounded-lg`}></div></div></li>
+
                 </ul>
                 <button className='text-lg mr-1 p-2 rounded-lg bg-blue-500 text-white'>Add to cart</button>
                 <button className="text-lg p-2 rounded-lg bg-gray-400 text-white" onClick={() => moreBtn(url)}>More</button>
--- src/app/plant-data.js
+++ src/app/plant-data.js
@@ -6,12 +6,16 @@ export const largeTomatos = [
         id: crypto.randomUUID,
         name: "earlygirl",
         price: price,
-        url: "https://seedsnsuch.com/products/new-girl-hybrid-tomato-seeds"
+        url: "https://seedsnsuch.com/products/new-girl-hybrid-tomato-seeds",
+        color: "blue",
+        colorCode: "blue-500"
     },
     {
         id: crypto.randomUUID,
         name: "other plant",
         price: price,
-        url: "https://en.wikipedia.org/wiki/Tomato"
+        url: "https://en.wikipedia.org/wiki/Tomato",
+        color: "red",
+        colorCode: "red-500"
     }
 ]
