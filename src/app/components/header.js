5b3cd8886a148a1703748c6e007167019ffdd181
isaiah <isaiahmartin847@gmail.com>
2024-03-20 09:18:40 -0600

    added button to the nave

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -1,12 +1,13 @@
 export default function NavBar () {
     return (
-        <nav className="bg-blue-500">
-            <div className="flex justify-between items-center">
-                <div className="flex item-center">
-                    <h1>NAME</h1>
+        <nav className="bg-blue-500 p-4">
+            <div className="flex justify-between items-center text-white">
+                <div className="flex item-center ml-4">
+                    <h1 className="font-extrabold text-xl">NAME</h1>
                 </div>
-                <div className="flex items-center">
-                    <a>Go to cart</a>
+                <div className="flex items-center mr-4 text-white">
+                    <a href="#" className="bg-blue-800 px-5 py-2 rounded mr-3">Cart</a>
+                    <a href="#" className="bg-blue-800 px-5 py-2 rounded">Home</a>
                 </div>
             </div>
         </nav>
