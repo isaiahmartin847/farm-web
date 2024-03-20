c5f6e4e80dfd66f0acfa476a264f1fc75554556b
isaiah <isaiahmartin847@gmail.com>
2024-03-20 09:28:04 -0600

    removed the gray boarders

--- src/app/components/plat-list.js
+++ src/app/components/plat-list.js
@@ -1,9 +1,9 @@
 export default function PlantList ({name, id, price}) {
     return ( 
-        <div key={id} className="border-2 border-red-600 m-0.5 flex flex-col justify-center items-center">
+        <div key={id} className="border-2  m-0.5 flex flex-col justify-center items-center">
             <div className="m-0.5 p-3">
                 <h1 className="mr-1 text-lg text-center" >{name}</h1>
-                <ul className="list-disc ml-5 pb-2 list-none">
+                <ul className="ml-5 pb-2">
                     <li>Price: {price}</li>
                     <li>test</li>
                 </ul>
