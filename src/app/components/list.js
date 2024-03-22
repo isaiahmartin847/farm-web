6b1fe256640e6dee55b7ded7f65fdb312be6ce2c
isaiahmartin <isaiahmartin847@gmail.com>
2024-03-22 14:10:37 -0600

    cleaned up the code for setting local stoarge

--- src/app/components/list.js
+++ src/app/components/list.js
@@ -7,8 +7,7 @@ import randomKey from "./random-key"
 
 export default function List () {
     const [items, setItems] = useState([])
-    const [cart, setCart] = useState([1])
-    const [storage, setStorage] = useState([])
+    const [cart, setCart] = useState([])
 
     useEffect(() => {
         setItems(() => {
@@ -21,19 +20,11 @@ export default function List () {
     useEffect(() => {
         const stoaredData = localStorage.getItem("cart")
         if (stoaredData){
-            // setStorage(() => {
-            //     return JSON.parse(stoaredData)
-            // })
-            setStorage((cart) => {
-                return [...cart]
-            })
              localStorage.setItem("cart", JSON.stringify(cart))
-            console.log("stoarge did exist but now i pulled it then spreaded the cart state then pushed the local stoarge again")
 
             
         } else {
             localStorage.setItem("cart", JSON.stringify(cart))
-            console.log("cart did not exist in local stoarge but now does")
         }
 
 
@@ -42,7 +33,6 @@ export default function List () {
     
     return(
         <>
-            {/* <h1> this is starge = {storage}</h1> */}
             {items.map(item => {
                 return (
                     < PlantList key={item.id} {...item} cart={cart} setCart={setCart}/> 
