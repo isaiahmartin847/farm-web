3340a84a31b91e0504bcbd749a2fc4379840dfea
isaiah <isaiahmartin847@gmail.com>
2024-03-27 13:01:00 -0600

    made it possible to display the cart items

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,13 +1,31 @@
 "use client"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NavBar from "../components/nav";
 
 export default function Cart() {
-    const [userCart, setUserCart] = useState([])
+    const [userCart, setUserCart] = useState([1, 3, 4])
+    const [cartCount, setCartCount] = useState({})
+
+    // useEffect((userCart) => {
+    //     setUserCart(() => {
+    //         return JSON.parse(localStorage.getItem('cart'))
+    //     })
+    // }, [])
+
+
+    // userCart.forEach((element) => {
+    //     console.log(element)
+    // })
+
+
     return (
         <>
-            <NavBar />
-            <h1>this is the cart</h1>
+            {/* <NavBar /> */}
+            {userCart.map(value => {
+                return (
+                    <h1>{value}</h1>
+                )
+            })}
         </>
     )
 }
