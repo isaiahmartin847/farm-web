a0d2c8928ef68c04fc92a5ba1b89af217dce4a94
isaiahmartin <isaiahmartin847@gmail.com>
2024-03-29 13:56:48 -0600

    added the remove item function

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -10,21 +10,34 @@ export default function Cart() {
     useEffect(() => {
         let counter = {}
         const cartItems = JSON.parse(localStorage.getItem('cart'))
-        cartItems.forEach(item => {
-            if(!counter[item]) {
-                counter[item] = 1
-            } else {
-                counter[item]++
-            }
-        })
-        setUserCart(() => {
-            return counter
-        })
+        if(!cartItems) {
+            console.log("nothing in local storage")
+            setUserCart({a: 2, b: 1})
+        } else {
+            cartItems.forEach(item => {
+                if(!counter[item]) {
+                    counter[item] = 1
+                } else {
+                    counter[item]++
+                }
+            })
+            setUserCart(() => {
+                return counter
+            })
+            localStorage.removeItem("cart")
+        }
+
     }, [])
 
 
-    const remove = (key) => {
-
+    const removeItem = (key) => {
+        console.log(`button clicked key: ${key}`)
+        Object.keys(userCart).map(itemKey => {
+            if(itemKey === key){
+                console.log(`found obj ${userCart[key]}`)
+            }
+        })
+    
     }
 
     return (
@@ -35,7 +48,7 @@ export default function Cart() {
                     <div key={key} className="border mb-4 flex-col justify-center text-center">
                         <h1>Name: {key}</h1>
                         <h2>Count: {userCart[key]}</h2>
-                        <button className="bg-red-500 rounded p-1 ">remove</button>
+                        <button className="bg-red-500 rounded p-1" onClick={() => {removeItem(key)}}>remove</button>
                     </div>
                 )
             })}            
