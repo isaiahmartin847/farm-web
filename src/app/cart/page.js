d99a8e3549d130ebc6313921cd745a7728527ddb
isaiah <isaiahmartin847@gmail.com>
2024-04-03 20:39:03 -0600

    added the cart object carting to the cart page

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -3,59 +3,53 @@ import { useEffect, useState } from "react";
 import NavBar from "../components/nav";
 
 export default function Cart() {
-    const [userCart, setUserCart] = useState({})
+    const [cart, setcart] = useState({})
 
 
 
     useEffect(() => {
-        let counter = {}
-        const cartItems = JSON.parse(localStorage.getItem('cart'))
-        if(!cartItems) {
-            console.log("nothing in local storage")
-            setUserCart({a: 2, b: 1})
-        } else {
-            cartItems.forEach(item => {
-                if(!counter[item]) {
-                    counter[item] = 1
-                } else {
-                    counter[item]++
-                }
-            })
-            setUserCart(() => {
-                return counter
-            })
-            // localStorage.removeItem("cart")
-        }
+        setcart((cart) => {
+            return {...cart, ...JSON.parse(localStorage.getItem("cart"))}
+        })
+
     }, [])
 
 
+
+    useEffect(() => {
+        localStorage.setItem("cart", JSON.stringify(cart))
+    }, [cart])
+
+
     const decermentItem  = (key) => {
-        if(userCart[key] === 1) {
-            const newCart = {...userCart}
+        if(cart[key] === 1) {
+            const newCart = {...cart}
             delete newCart[key]
-            setUserCart(newCart)
+            setcart(newCart)
         } else {
-            setUserCart(userCart => ({
-                ...userCart,
-                [key]: userCart[key] - 1
+            setcart(cart => ({
+                ...cart,
+                [key]: cart[key] - 1
             }))
         }
     }
 
 
     const removeItem = (key) => {
-        const tempCart = {...userCart}
+        const tempCart = {...cart}
         delete tempCart[key]
-        setUserCart(tempCart) 
+        setcart(tempCart) 
     }
+
+
     return (
         <>
             <NavBar />
-            {Object.keys(userCart).map(key => {
+            {Object.keys(cart).map(key => {
                 return (
                     <div key={key} className="border mb-4 flex-col justify-center text-center">
                         <h1>Name: {key}</h1>
-                        <h2>Count: {userCart[key]}</h2>
+                        <h2>Count: {cart[key]}</h2>
                         <button className="bg-red-500 rounded p-1 mr-1" onClick={() => {decermentItem(key)}}>remove One</button>
                         <button className="bg-red-500 rounded p-1" onClick={() => {removeItem(key)}}>Remove All</button>
                     </div>
