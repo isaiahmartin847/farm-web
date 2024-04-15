d59adb045b73419f07aa927b9e1869132644abb0
isaiah <isaiahmartin847@gmail.com>
2024-04-15 08:19:03 -0600

    added the data base conection and now you are able to query things from the db

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,32 @@
 
 
 const express = require("express")
-const app = express()
+const mysql = require("mysql")
 const cors = require("cors")
+// const router = express.Router();
+const app = express()
 const PORT = 8080
 const { largeTomatoes, smallTomatoes, peppersData } = require('./plant_data')
 
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test_cart'
+}); 
+
+
+connection.connect((err) => {
+    if (err) {
+        console.error(`error connections to the database failed ${err.stack}`)
+    }
+    console.log(`connected to the server id ${connection.threadId}`)
+});
+
+// this exports the database 
+// module.exports = connection;
 
 
 app.use(cors())
@@ -21,6 +42,19 @@ app.get("/api/tomato/small", (req, res) => {
 app.get("/api/pepper", (req, res) => {
     res.json({peppersData})
 })
+
+app.get('/users', (req, res) => {
+    // Example query to retrieve users from MySQL
+    connection.query('SELECT * FROM Users', (err, results) => {
+        console.log(results)
+        if (err) {
+            console.log(results)
+            console.log('Error executing MySQL query: ' + err.stack);
+            return res.status(500).json({ error: 'Database error' });
+        }
+        res.json(results);
+    });
+});
 
 
 app.listen(PORT, () => {
