eec84e3434cf83ae69216e3987193fa04938c3b5
isaiah <isaiahmartin847@gmail.com>
2024-04-16 14:58:37 -0600

    added some styling to the static plants js

--- Client/src/app/components/show_plant.js
+++ Client/src/app/components/show_plant.js
@@ -1,20 +1,22 @@
-
+import Price from "./price"
 
 
 export default function ShowPlant ({name, type, species, price, description}) {
 
     
   
-    return ( 
-        <div className="border-b-2  m-0.5 flex flex-col justify-center items-center">
+    return (
+        <div className="border-b-2  m-0.5 flex flex-col justify-center items-center ">
             <div  className="m-0.5 p-3 w-[300px]">
-                <h1  className="mr-1 text-lg text-center" >Name: {name}</h1>
-                <ul  className="ml-5 pb-2">
-                    <li >Price: {`$${price}`}</li>
-                    <li ><div className="flex flex-row">Color: blue   <div className={` h-4 w-4 rounded-lg bg-blue-200 mt-1 ml-1`}></div></div></li>
-                    <li>description: {description}</li>
-                </ul>
-              </div>
+                <div className="ml-10">
+                    <h1  className="mr-1 text-lg" >Name: {name}</h1>                    
+                    <ul  className="ml-5 pb-2">
+                        <Price price={4.1}/>
+                        <li ><div className="flex flex-row">Color: blue   <div className={` h-4 w-4 bg-blue-500 rounded-lg bg-blue-200 mt-1 ml-1`}></div></div></li>
+                        <li>description: N/A</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
