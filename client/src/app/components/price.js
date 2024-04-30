948bc79dbe08a43a37715e0d6260b88e77dd661c
isaiah <isaiahmartin847@gmail.com>
2024-04-30 11:09:05 -0600

    added the price update and added the functionality to manage decimals

--- client/src/app/components/price.js
+++ client/src/app/components/price.js
@@ -1,10 +1,18 @@
 export default function Price ({price}) {
     if(price % 2 === 0) {
-    return (
-        <li className="font-bold">Price: <span className="italic">${price}.00</span></li>
-    )} else {
+        console.log(price)
+        return (
+            <li className="font-bold">Price: <span className="italic">${price}.00</span></li>
+        )
+    } else if (price.length === 3) {
+        console.log(price)
         return (
             <li className="font-bold">Price: <span className="italic">${price}0</span></li>
+        )
+    } else {
+        console.log(price.length) 
+        return (
+            <li className="font-bold">Price: <span className="italic">${price}</span></li>
         )
     }
 }
