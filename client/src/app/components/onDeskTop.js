832d4e9c45546a8b22e84445a24273403ab1f442
isaiah <isaiahmartin847@gmail.com>
2024-05-29 16:24:57 -0600

    added the qr code and the button to ignore

--- client/src/app/components/onDeskTop.js
+++ client/src/app/components/onDeskTop.js
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react"
+import Image from 'next/image'
+import farmwebqrcode from '/public/farmwebqrcode.png'
 
 
 
@@ -24,8 +26,17 @@ export default function OnDeskTop() {
     return (
         <div className="w-screen h-screen border-black border-2">
             <h1 className="text-center mt-4 font-medium text-2xl">This application is made for mobile</h1>
+            <div className="flex justify-center items-center m-10 flex-col">
+                
+                <Image 
+                    src="/farmwebqrcode.png"
+                    width={300}
+                    height={300}
+                /> 
+                <h1 className="mt-2">Scan me on your phone</h1>
+            </div>
             <div className="justify-center text-center content-center mt-2">
-                <button className="border-black border-2 rounded-lg bg-blue-500 text-white font-semibold p-2 hover:bg-blue-400" onClick={() => {toggleMsg()}}>proceed to the app</button>
+                <button className="border-black border-2 rounded-lg bg-blue-500 text-white font-semibold p-2 hover:bg-blue-400" onClick={() => {toggleMsg()}}>proceed to the app anyway</button>
             </div>
         </div>
     )
